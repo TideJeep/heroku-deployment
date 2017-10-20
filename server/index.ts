54d6511efac8085ce2817fe165f82b60c184a656
Brad Barnett <bbarnett@Brads-MacBook-Air.local>
2017-10-20 15:50:03 -0500

    Prepared for heroku deployment

--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,6 @@ app.use(express.static(clientPath)); //create static server, where are the files
 
 console.log(process.env.GREETING); //look for variable called GREETING in this environment
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
